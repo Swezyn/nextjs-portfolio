eecf54fbaa29d3d7f86b205d0434daf72c7fddfb
Swezyn <akessonelias@gmail.com>
2022-08-12 23:28:02 +0200

    more mobile

--- components/About.tsx
+++ components/About.tsx
@@ -25,8 +25,8 @@ export default function About() {
       {/* Scroll down arrow */}
       <BsArrowDown style={{ opacity: scrollY < 600 ? 0 : 1}} className='animate-bounce h-16 w-12 -z-10 transition-opacity' />
       <div style={{transform: `scale(${scrollY < 600 && scrollY > -400 ? 100 : 50}%)`, minWidth: "18rem"}} className='mt-8 w-3/5 max-w-screen-lg transition-transform duration-500 items-center lg:flex hidden lg:flex-col'>
-        <div style={{transform: `translateX(${scrollY < 400 && scrollY > -400 ? -50 : 0}%) scale(75%)`}} className='h-72 lg:h-96 w-fit aspect-square bg-neutral-700 absolute rounded-xl overflow-hidden transition-transform duration-500'>
-          <img src="/me.jpeg" className="w-full h-full object-cover"/>
+        <div style={{transform: `translateX(${scrollY < 400 && scrollY > -400 ? -70 : 0}%) scale(75%)`}} className='h-72 lg:h-96 w-fit aspect-square bg-neutral-700 absolute rounded-xl overflow-hidden transition-transform duration-500'>
+          <img src="/me.JPG" className="w-full h-full object-cover scale-150"/>
         </div>
         <div style={{transform: `translateX(${scrollY < 400 && scrollY > -400 ? 30 : 0}%)`}} className='h-72 lg:h-96 w-4/6 bg-neutral-100 rounded-xl p-8 shadow-2xl transition-transform duration-500'>
           <p className='text-xs leading-relaxed lg:text-xl'><span className='text-sm lg:text-2xl font-bold bg-purple-400 py-1 px-6 rounded-xl text-neutral-50 '>Hi!</span> I am a 17 year old boy from Sweden, who loves programming.</p>
@@ -36,11 +36,11 @@ export default function About() {
           <p className='text-xs leading-relaxed lg:text-xl'>When I'm not coding, I ride mountain bikes or go skiing.</p>
         </div>
       </div>
-      <div style={{transform: `scale(${scrollY < 600 && scrollY > -400 ? 100 : 50}%) translateY(-30%)`}} className='mt-32 w-4/5 max-w-screen-lg transition-transform duration-500 items-center lg:hidden flex flex-col'>
-        <div style={{transform: `translateY(${scrollY < 400 && scrollY > -400 ? -70 : -20}%) scale(50%)`}} className='h-72 lg:h-96 w-fit aspect-square bg-neutral-700 absolute rounded-xl overflow-hidden transition-transform duration-500'>
-          <img src="/me.jpeg" className="w-full h-full object-cover"/>
+      <div style={{transform: `scale(${scrollY < 600 && scrollY > -400 ? 100 : 50}%) translateY(-20%)`}} className='mt-32 w-4/5 max-w-screen-lg transition-transform duration-500 items-center lg:hidden flex flex-col'>
+        <div style={{transform: `translateY(${scrollY < 400 && scrollY > -400 ? -70 : -20}%) scale(50%)`}} className='h-72 lg:h-96 w-72 lg:w-96 bg-neutral-700 absolute rounded-xl overflow-hidden transition-transform duration-500'>
+          <img src="/me.JPG" className="w-full h-full object-cover scale-150"/>
         </div>
-        <div className='h-72 lg:h-96 w-full bg-neutral-100 rounded-xl p-8 shadow-2xl transition-transform duration-500 z-10'>
+        <div style={{transform: `translateY(${scrollY < 400 && scrollY > -400 ? 0 : -20}%)`}} className='h-72 lg:h-96 w-full bg-neutral-100 rounded-xl p-8 shadow-2xl transition-transform duration-500 z-10'>
           <p className='text-xs leading-relaxed lg:text-xl'><span className='text-sm lg:text-2xl font-bold bg-purple-400 py-1 px-6 rounded-xl text-neutral-50 '>Hi!</span> I am a 17 year old boy from Sweden, who loves programming.</p>
           <p className='overflow-hidden text-xl tracking-widest leading-10 h-10 text-neutral-400'>{[...Array(80)].map((e, i) => ("-"))}</p>
           <p className='text-xs leading-relaxed lg:text-xl'>Coding has been a passion of mine for nearly 5 years, and I enjoy creating beautiful and functional websites.</p>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -20,8 +20,10 @@ export default function Skills() {
             <svg style={{transform: `translate(5%, -25%) scale(${scrollY < 400 ? 120 : 0}%)`, transitionDelay: `${scrollY > 400 ? 200 : 0}ms`, maxWidth: "70vw"}} className="transition-transform duration-500 absolute -z-10 overflow-hidden" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
               <path className='fill-purple-400 opacity-30' d="M26.8,-39.8C38.8,-39.6,55.1,-40.4,57.3,-34C59.6,-27.5,47.8,-13.7,46.9,-0.5C46.1,12.8,56.4,25.6,54.5,32.7C52.6,39.8,38.5,41.1,27.4,39.5C16.3,37.8,8.2,33.2,-3.7,39.6C-15.5,45.9,-31,63.2,-44.7,66.3C-58.4,69.5,-70.3,58.5,-78.2,45C-86.2,31.6,-90.2,15.8,-87.9,1.3C-85.5,-13.1,-76.9,-26.2,-64.9,-32.7C-52.9,-39.1,-37.5,-38.7,-26.2,-39.2C-14.8,-39.7,-7.4,-41,0,-41.1C7.5,-41.1,14.9,-40,26.8,-39.8Z" transform="translate(100 100)" />
             </svg>
-            <h1 style={{transform: `translateY(${scrollY < 400 ? 0 : -120}%)`, transitionDelay: `${scrollY < 400 ? 100 : 0}ms`}} className='lg:text-7xl text-2xl font-bold text-neutral-900 leading-none transition-transform duration-500'>My most important skills</h1>
-            <p style={{transform: `translateY(${scrollY < 400 ? 0 : -500}%)`}} className='leading-normal lg:text-2xl lg:mt-8 mt-4 transition-transform duration-500'>These are my favorite tools and frameworks for <span className='font-semibold'>Web Development</span></p>
+            <div className='lg:translate-y-0 translate-y-8'>
+              <h1 style={{transform: `translateY(${scrollY < 400 ? 0 : -250}%)`, transitionDelay: `${scrollY < 400 ? 100 : 0}ms`}} className='lg:text-7xl text-2xl font-bold text-neutral-900 leading-none transition-transform duration-500'>My most important skills</h1>
+              <p style={{transform: `translateY(${scrollY < 400 ? 0 : -400}%)`}} className='leading-normal lg:text-2xl lg:mt-8 mt-4 transition-transform duration-500 delay-100'>These are my favorite tools and frameworks for <span className='font-semibold'>Web Development</span></p>
+            </div>
           </div>
           <div className='lg:w-1/2'>
             <Skill name="React JS" desc="As someone who has used React for the majority of my career, I would consider myself very knowledgeable in the area." procent="75" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" />
@@ -53,7 +55,7 @@ function Skill({name, desc, procent, src}) {
   return (
     <div ref={ref} className='bg-neutral-100 h-fit rounded-xl flex flex-col items-center shadow-xl lg:p-16 p-8'>
       <div className='flex w-full lg:mb-16 mb-8'>
-        <div style={{transform: `scale(${scrollY < 650 ? 100 : 0}%)`}} className='aspect-square lg:h-16 h-10 transition-transform duration-300'>
+        <div className={`aspect-square lg:h-16 h-10 transition-transform duration-300 ${scrollY < 650 ? "scale-100" : "scale-0"}`}>
           <img className='h-full w-full' src={src} />
         </div>
         <div style={{transform: `translateX(${scrollY < 650 ? 0 : -5}rem)`}} className='flex flex-col lg:ml-8 ml-4 transition-transform duration-300' >
@@ -65,7 +67,7 @@ function Skill({name, desc, procent, src}) {
       <h2 style={{opacity: scrollY < 650 ? 1 : 0}} className=' font-semibold lg:text-xl text-sm text-neutral-700 w-full leading-loose transition-opacity duration-300'>Better than</h2>
       <div className='w-full lg:h-4 h-2 border-neutral-500 bg-neutral-400 border-2 rounded-full flex items-center'>
         <div style={{width: `${scrollY < 650 ? procent : 0}%`}} className='flex justify-end transition-all duration-500'>
-          <div className='aspect-square rounded-full lg:h-8 h-4 bg-purple-400 border-neutral-500 border-2 translate-x-4'>
+          <div className='rounded-full lg:h-8 lg:w-8 h-4 w-4 bg-purple-400 border-neutral-500 border-2 translate-x-4'>
             <p style={{opacity: scrollY < 650 ? 1 : 0}} className='-translate-y-8 font-bold lg:text-base text-xs text-neutral-700 text-center transition-opacity'>{procent}%</p>
           </div>
         </div>
