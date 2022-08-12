5fdd3c8195f25c8a1bff4d3f03699117c4fd671e
Swezyn <akessonelias@gmail.com>
2022-08-12 23:00:43 +0200

    might fix it

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }) {
     <>
         <Head>
             <title>Elias Akesson</title>
-            <meta name="viewport" content="maximum-scale=1.0" />
+            <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
             <meta name="description" content="Elias Akesson is a web developer and designer from Sweden. He has a passion for creating beautiful and functional websites." />
         </Head>
         <Component {...pageProps} />
